ff6bbb35da07a536f68f9604335afd59199f2983
yujeonghyeon <dbdb1114@naver.com>
2022-08-30 00:28:09 +0900

    no message

--- src/bj1940.java
+++ src/bj1940.java
@@ -50,28 +50,3 @@ public class bj1940 {
 
 }
 
-class Num {
-	
-	BufferedReader br;
-	StringTokenizer stringTokenizer;
-
-	
-	void recordnum() throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
-	}
-	
-	void printnum() throws IOException {	
-		
-	
-	stringTokenizer = new StringTokenizer(br.readLine());
-
-	}
-	 
-	int i() {	return Integer.parseInt(stringTokenizer.nextToken());	}
-	
-	String s() { return stringTokenizer.nextToken(); }
-	
-	void bfclose() throws IOException{
-		br.close();
-	}
-}
